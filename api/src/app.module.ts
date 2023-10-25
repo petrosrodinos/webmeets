c73dab3b3bcfd174b7d30106852529821f7a11ce
Petros <petros.rodinos@yahoo.com>
2023-10-25 22:26:23 +0300

    added mongoose

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,16 +5,21 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
     UserModule,
     BookmarkModule,
     PrismaModule,
+    BookModule,
   ],
 })
 export class AppModule {}
