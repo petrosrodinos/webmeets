565390698f7d8fcd223ca5ac90daaf2f27d79504
Petros <petros.rodinos@yahoo.com>
2024-01-28 20:50:43 +0200

    joining room

---
+++ client/src/app/meet/index.tsx
@@ -0,0 +1,18 @@
+// 'use client';
+// import { useEffect, useRef, useState } from 'react';
+// import Daily from '@daily-co/daily-js';
+// import { DailyProvider, DailyVideo } from '@daily-co/daily-react';
+
+// const Meet2 = () => {
+//   // const co = Daily.createCallObject({
+//   //   url: 'https://webmeets.daily.co/tbvAWCw5PsRw0Ff7ATnk',
+//   // });
+
+//   return (
+//     <DailyProvider url="https://webmeets.daily.co/qa07ZiVavtneCcffCux7">
+//       <DailyVideo automirror sessionId={''} type={'video'} />
+//     </DailyProvider>
+//   );
+// };
+
+// export default Meet2;
